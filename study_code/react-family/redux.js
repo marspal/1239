eb42c3dc87b756e8a40edf134ae02584bf4ceeb2
andyxu <xumingquan01@baidu.com>
2020-11-19 20:57:49 +0800

    feat: 对createStore进行中间件的增强

--- study_code/react-family/redux.js
+++ study_code/react-family/redux.js
@@ -3,12 +3,16 @@
  * @author andyxu
  * to: reducer拆分
  */
-
+// 日志dispatch action type: 传递 是否需要上传日志
 module.exports = {
     createStore,
-    combineReducer
+    combineReducer,
+    applyMiddleware
 };
-function createStore(reducer, initState) {
+function createStore(reducer, initState, rewriteCreateStoreFunc) {
+    if (rewriteCreateStoreFunc) {
+        return rewriteCreateStoreFunc(createStore)(reducer, initState);
+    }
     let state = initState;
     let listeners = [];
     function subscribe(listener) {
@@ -42,5 +46,18 @@ function combineReducer(reducers) {
             nextState[key] = reducer(prevState, action);
         });
         return nextState;
+    };
+}
+/* 接收旧的 createStore，返回新的 createStore */
+function applyMiddleware(...middlwares) {
+    return function rewriteCreateStoreFunc(oldCreateStore) {
+        return function newCreateStore(reducer, initState) {
+            const store = createStore(reducer, initState);
+            const chain = middlwares.map(middleware => middleware(store));
+            store.dispatch = chain.reduceRight((current, next) => {
+                return next(current);
+            }, store.dispatch);
+            return store;
+        };
     };
 }
