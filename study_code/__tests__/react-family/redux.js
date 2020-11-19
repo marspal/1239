c2c7c465c75b1b100d078bfa0fb96e3994802ad5
andyxu <xumingquan01@baidu.com>
2020-11-19 23:02:03 +0800

    no mess

--- study_code/__tests__/react-family/redux.js
+++ study_code/__tests__/react-family/redux.js
@@ -36,76 +36,81 @@ function infoReducer(state = stateInfo, action) {
             return state;
     }
 }
-describe('redux', () => {
-    it('should return the initState', () => {
-        const store = createStore(changeCountReducer, initState);
-        expect(store.getState().count).toEqual(1);
-    });
-    it('should return the correct state after called the dispatch with param {type: "INCREMENT"}', () => {
-        const store = createStore(changeCountReducer, initState);
-        store.dispatch({type: 'INCREMENT'});
-        expect(store.getState().count).toBe(2);
-    });
-    it('should count decrement minus 1 with {type: "DECREMENT"}', () => {
-        const store = createStore(changeCountReducer, initState);
-        store.dispatch({type: 'DECREMENT'});
-        expect(store.getState().count).toBe(0);
-    });
-    it('listener should be called when change state', () => {
-        const store = createStore(changeCountReducer, initState);
-        const listener = jest.fn(() => {
-            const state = store.getState();
-            expect(state).toEqual(state);
+describe('Redux', () => {
+    describe('redux createStore', () => {
+        it('should return the initState', () => {
+            const store = createStore(changeCountReducer, initState);
+            expect(store.getState().count).toEqual(1);
         });
-        store.subscribe(listener);
-        store.dispatch({type: 'DECREMENT'});
-        expect(listener).toHaveBeenCalled();
+        it('should return the correct state after called the dispatch with param {type: "INCREMENT"}', () => {
+            const store = createStore(changeCountReducer, initState);
+            store.dispatch({type: 'INCREMENT'});
+            expect(store.getState().count).toBe(2);
+        });
+        it('should count decrement minus 1 with {type: "DECREMENT"}', () => {
+            const store = createStore(changeCountReducer, initState);
+            store.dispatch({type: 'DECREMENT'});
+            expect(store.getState().count).toBe(0);
+        });
+        it('listener should be called when change state', () => {
+            const store = createStore(changeCountReducer, initState);
+            const listener = jest.fn(() => {
+                const state = store.getState();
+                expect(state).toEqual(state);
+            });
+            store.subscribe(listener);
+            store.dispatch({type: 'DECREMENT'});
+            expect(listener).toHaveBeenCalled();
+        });
+        it('should work after use combineReducer', () => {   
+            const reducer = combineReducer({
+                counter: changeCountReducer,
+                info: infoReducer
+            });  
+            const newInitState = {
+                counter: initState,
+                info: stateInfo
+            };
+            const store = createStore(reducer, newInitState);
+            expect(store.getState().counter.count).toBe(1);
+            expect(store.getState().info.message).toBe('andyxu');
+    
+            store.dispatch({
+                type: 'INCREMENT'
+            });
+            expect(store.getState().counter.count).toBe(2);
+            expect(store.getState().info.message).toBe('andyxu');
+    
+            store.dispatch({
+                type: 'CHANGE_NAME',
+                message: 'tomorrow will be fine!'
+            });
+            expect(store.getState().counter.count).toBe(2);
+            expect(store.getState().info.message).toBe('tomorrow will be fine!');
+        });
+        it('should work without initial State', () => {
+            const reducer = combineReducer({
+                counter: changeCountReducer,
+                info: infoReducer
+            });
+            const store = createStore(reducer);
+            expect(store.getState()).not.toBeUndefined();
+            store.dispatch({
+                type: 'CHANGE_NAME',
+                message: 'another example'
+            });
+            expect(store.getState().info.message).toBe('another example');
+        });
+        it('should work after spilting state', () => {
+            const reducer = combineReducer({
+                counter: changeCountReducer,
+                info: infoReducer
+            });
+            const store = createStore(reducer);
+            expect(store.getState().counter.count).toBe(1);
+        });
     });
-    it('should work after use combineReducer', () => {   
-        const reducer = combineReducer({
-            counter: changeCountReducer,
-            info: infoReducer
-        });  
-        const newInitState = {
-            counter: initState,
-            info: stateInfo
-        };
-        const store = createStore(reducer, newInitState);
-        expect(store.getState().counter.count).toBe(1);
-        expect(store.getState().info.message).toBe('andyxu');
+    describe('redux applyMiddleware', () => {
 
-        store.dispatch({
-            type: 'INCREMENT'
-        });
-        expect(store.getState().counter.count).toBe(2);
-        expect(store.getState().info.message).toBe('andyxu');
-
-        store.dispatch({
-            type: 'CHANGE_NAME',
-            message: 'tomorrow will be fine!'
-        });
-        expect(store.getState().counter.count).toBe(2);
-        expect(store.getState().info.message).toBe('tomorrow will be fine!');
-    });
-    it('should work without initial State', () => {
-        const reducer = combineReducer({
-            counter: changeCountReducer,
-            info: infoReducer
-        });
-        const store = createStore(reducer);
-        expect(store.getState()).not.toBeUndefined();
-        store.dispatch({
-            type: 'CHANGE_NAME',
-            message: 'another example'
-        });
-        expect(store.getState().info.message).toBe('another example');
-    });
-    it('should work after spilting state', () => {
-        const reducer = combineReducer({
-            counter: changeCountReducer,
-            info: infoReducer
-        });
-        const store = createStore(reducer);
-        expect(store.getState().counter.count).toBe(1);
     });
 });
